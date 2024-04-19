519ba8fe7ae8e4116758e541effa5e4907cbab41
ihua <r11922136@csie.ntu.edu.tw>
2024-04-19 15:37:25 +0800

    multiple model and dataset

--- tools/hp5dataset.py
+++ tools/hp5dataset.py
@@ -2,19 +2,44 @@ import h5py
 import os
 import torch
 import pickle
+import csv
 from collections import defaultdict
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, csv = None, info=False, train=True, demo=False):
+    def __init__(self, data_dir, csv_file=None, info=False, type="train", demo=False):
         self.hdf5_file = data_dir
         self.file = h5py.File(self.hdf5_file, 'r')
-        self.keys = list(self.file.keys())
-        split_index = int(len(self.keys) * 0.8) if len(self.keys) != 1 else 1
+        all_keys = list(self.file.keys())
 
-        if train:
-            self.keys = self.keys[:split_index]
+        if csv_file is not None:
+            csv_keys = set()
+            with open(csv_file, newline='', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    app_name, trace_id, _ = row
+                    formatted_key = f"{app_name.replace('.', '_')}_{trace_id.replace(',', '_')}"
+                    csv_keys.add(formatted_key)
+
+            self.test_keys = [key for key in all_keys if any(csv_key in key for csv_key in csv_keys)]
+            other_keys = [key for key in all_keys if key not in self.test_keys].sort()
+            self.training_keys = other_keys[:int(0.8 * len(other_keys))]
+            self.validation_keys = other_keys[int(0.8 * len(other_keys)):]
         else:
-            self.keys = self.keys[split_index:]
+            # sort the keys
+            all_keys.sort()
+            self.training_keys = all_keys[:int(0.8 * len(all_keys))]
+            self.validation_keys = all_keys[int(0.8 * len(all_keys)):int(0.95 * len(all_keys))]
+            self.test_keys = all_keys[int(0.95 * len(all_keys)):]
+
+        if type == "train":
+            self.keys = self.training_keys
+        elif type == "val":
+            self.keys = self.validation_keys
+        elif type == "test":
+            self.keys = self.test_keys
+
+        if demo:
+            self.keys = self.keys[:1000]
 
     def __len__(self):
         return len(self.keys)
@@ -34,25 +59,60 @@ class Dataset(torch.utils.data.Dataset):
 
 
 class MultiFileDataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, csv = None, info=False, train=True, demo=False):
+    def __init__(self, data_dir, csv_file=None, info=False, type="train", demo=False):
         self.data_dir = data_dir
         # List all .h5 files in the specified directory
         self.files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith('.h5')]
+        self.val_dataset_keys = []
+        self.train_dataset_keys = []
+        self.test_dataset_keys = []
         self.dataset_keys = []
+
         # Extract keys from all files
-        for file_path in self.files:
-            with h5py.File(file_path, 'r') as file:
-                self.dataset_keys.extend([(file_path, key) for key in file.keys()])
-        
-        if demo:
-            self.dataset_keys = self.dataset_keys[:1000]
-        
-        # Split the dataset into training and testing based on an 80/20 split
-        split_index = int(len(self.dataset_keys) * 0.8)
-        if train:
-            self.dataset_keys = self.dataset_keys[:split_index]
+        if csv_file is not None:
+            csv_keys = set()
+            with open(csv_file, newline='', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    app_name, trace_id, id = row
+                    formatted_key = f"{app_name}_{trace_id}_{id}"
+                    csv_keys.add(formatted_key)
+            
+            other_keys = []
+            for file_path in self.files:
+                with h5py.File(file_path, 'r') as file:
+                    for key in file.keys():
+                        if any(csv_key in key for csv_key in csv_keys):
+                            self.test_dataset_keys.append((file_path, key))
+                        else:
+                            other_keys.append((file_path, key))
+            
+            other_keys = sorted(other_keys, key=lambda x: x[0])
+            split_index = int(len(other_keys) * 0.8)
+            self.train_dataset_keys = other_keys[:split_index]
+            self.val_dataset_keys = other_keys[split_index:]
+            
         else:
-            self.dataset_keys = self.dataset_keys[split_index:]
+            for file_path in self.files:
+                with h5py.File(file_path, 'r') as file:
+                    self.dataset_keys.extend([(file_path, key) for key in file.keys()])
+           
+            self.dataset_keys.sort(key=lambda x: x[0])
+            self.train_dataset_keys = self.dataset_keys[:int(len(self.dataset_keys) * 0.8)]
+            self.val_dataset_keys = self.dataset_keys[int(len(self.dataset_keys) * 0.8):int(len(self.dataset_keys) * 0.95)]
+            self.test_dataset_keys = self.dataset_keys[int(len(self.dataset_keys) * 0.95):]
+
+        if demo:
+            self.train_dataset_keys = self.train_dataset_keys[:1000]
+            self.val_dataset_keys = self.val_dataset_keys[:500]
+        
+        if type == "train":
+            self.dataset_keys = self.train_dataset_keys
+        elif type == "val":
+            self.dataset_keys = self.val_dataset_keys
+        elif type == "test":
+            self.dataset_keys = self.test_dataset_keys
+        
 
     def __len__(self):
         return len(self.dataset_keys)
@@ -93,8 +153,10 @@ def point_collate_fn(batch):
                 batched_image_frames.append(image_frames)
                 max_val, flat_index = torch.max(heatmap.flatten(), 0)
                 dims = heatmap.shape
-                max_index = (flat_index // (dims[1] * dims[2]), (flat_index % (dims[1] * dims[2])) // dims[2], flat_index % dims[2])
-
+                max_index = [flat_index // (dims[1] * dims[2]), (flat_index % (dims[1] * dims[2])) // dims[2], flat_index % dims[2]]
+                # normalize the point
+                max_index[1] = max_index[1] / dims[1]
+                max_index[2] = max_index[2] / dims[2]
                 # Converting index to a floating-point tensor
                 max_index_float = torch.tensor(max_index, dtype=torch.float)
                 batched_point.append(max_index_float[1:3])
--- train_rico.sh
+++ train_rico.sh
@@ -5,10 +5,10 @@ dataset_path="/data/rico"
 save_path="/data/model"
 epochs=50
 lr=0.0001
-csv_path="./exp/pick.csv"
+csv_path="./valid.csv"
 
 # model_names=("VLModel" "VL2DModel" "UNet") # List of model names
-model_names=("VL2DModel") # List of model names
+model_names=("VLModel") # List of model names
 
 # Loop over models
 for model_name in "${model_names[@]}"
@@ -21,15 +21,15 @@ do
         val_batch_size=8
     elif [ "$model_name" = "VL2DModel" ]; then
         batch_size=32
-        val_batch_size=32
+        val_batch_size=64
     elif [ "$model_name" = "UNet" ]; then
         batch_size=8
         val_batch_size=32
     elif [ "$model_name" = "UNet2D" ]; then
-        batch_size=8
+        batch_size=32
         val_batch_size=32
     elif [ "$model_name" = "UNet3D" ]; then
-        batch_size=8
+        batch_size=16
         val_batch_size=32
     fi
     # Loop over loss_alpha and loss_gamma values
@@ -47,10 +47,9 @@ do
                 --loss_alpha ${loss_alpha} \
                 --loss_gamma ${loss_gamma} \
                 --save_path ${save_path} \
-                --csv_path ${csv_path} \
-                --decoder "point" \
+                --decoder "heatmap" \
                 --val_batch_size ${val_batch_size} \
-                --test 1 \
+                --csv_path ${csv_path} \
                 --gpu 0
         done
     done
